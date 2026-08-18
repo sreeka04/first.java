54f0a342230f41457795498d2669b6dc2923f72e
sreeka <254095865+sreeka04@users.noreply.github.com>
2026-08-18 23:12:34 +0530

    Add files via upload

--- age.java
+++ age.java
@@ -1,18 +1,19 @@
-public class Age {
+import java.util.Scanner;
+
+public class age {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine(); 
+        String name = scanner.nextLine();
 
         System.out.print("Enter your age: ");
-        int age = scanner.nextInt(); 
+        int age = scanner.nextInt();
+
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
 
         scanner.close();
     }
-
-    
 }
